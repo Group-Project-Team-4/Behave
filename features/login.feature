2394ebe5bed0788502455bff9b11ba202732ec0b
Nathaniel Schainblatt <nschainblatt@gmail.com>
2023-11-11 20:01:39 -0500

    Login test

---
+++ features/login.feature
@@ -0,0 +1,7 @@
+Feature: Login functionality on the clothing store app
+
+  Scenario: User can login
+     Given User is on the shop page
+      When User clicks login
+      When User enters login credentials
+      When User logs in
---
+++ features/steps/login.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from behave import *
+import time
+
+USERNAME = 'nate123'
+PASSWORD = '12345'
+
+
+@given('User is on the shop page')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+    context.driver.get('http://localhost:5000/')
+    assert context.driver.current_url == 'http://localhost:5000/'
+    context.driver.save_screenshot("./login/main_page.png")
+
+
+@when('User clicks login')
+def step_impl(context):
+    login_link = context.driver.find_element(By.ID, "base_login_anchor")
+    login_link.click()
+    time.sleep(0.25)
+    assert context.driver.current_url == 'http://localhost:5000/auth/login'
+    context.driver.save_screenshot("./login/login_page.png")
+
+
+@when('User enters login credentials')
+def step_impl(context):
+    username_input = context.driver.find_element(By.ID, "login_username_textinput")
+    username_input.send_keys(USERNAME)
+    time.sleep(0.25)
+    assert username_input.get_attribute('value') == 'nate123'
+    password_input = context.driver.find_element(By.ID, "login_password_textinput")
+    password_input.send_keys(PASSWORD)
+    time.sleep(0.25)
+    assert password_input.get_attribute('value') == '12345'
+    context.driver.save_screenshot("./login/login_credentials.png")
+
+
+@when('User logs in')
+def step_impl(context):
+    login_button = context.driver.find_element(By.ID, "login_submit_button")
+    login_button.click()
+    time.sleep(0.25)
+    assert context.driver.current_url == 'http://localhost:5000/'
+    context.driver.save_screenshot("./login/login_success.png")
+
